0d2a02e8304de02907d7bbf657f2b8670f2358ab
Saba Ahmad <sabaahmad895@gmail.com>
2023-03-01 23:45:02 +0500

    added data and schema data

--- schema.sql
+++ schema.sql
@@ -1,5 +1,10 @@
-/* Database schema to keep the structure of entire database. */
+CREATE TABLE animals
+(
+    id integer,
+    name varchar,
+    date_of_birth date,
+    escape_attempts integer,
+    neutered boolean,
+    weight_kg integer
+)
 
-CREATE TABLE animals (
-    name varchar(100)
-);
