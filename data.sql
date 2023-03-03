0ac3f33111ad959f0acaf74103194fb38fef1dee
Saba Ahmad <sabaahmad895@gmail.com>
2023-03-04 01:16:46 +0500

    one-to-many relationship

--- data.sql
+++ data.sql
@@ -11,4 +11,58 @@ INSERT INTO animals VALUES (8, 'Angemon', '06-12-2005', 3, false , -45);
 INSERT INTO animals VALUES (9, 'Boarmon', '06-07-2005', 7, true , 20.4);
 INSERT INTO animals VALUES (10, 'Blossom', '10-13-1998', 3, true , 17);
 INSERT INTO animals VALUES (11, 'Ditto', '05-14-2022', 4, true , 22);
+-- owners table data
+INSERT INTO owners (full_name, age) VALUES ( 'Sam Smith ', 34);
+INSERT INTO owners (full_name, age) VALUES ( 'Jennifer Orwell', 19),( 'Bob', 45),('Melody Pond',77),('Dean Winchester',14),('Jodie Whittaker',38)
+
+-- species table data
+
+INSERT INTO species (name) VALUES ('Pokemon'),('Digimon')
+
+
+
+                
+    --  insertion and quries
+
+-- Modify your inserted animals so it includes the species_id value:
+-- If the name ends in "mon" it will be Digimon
+update animals
+set species_id=(select id from species where name='Digimon')
+where name like '%mon'
+
+-- All other animals are Pokemon
+
+update animals
+set species_id=(select id from species where name='Pokemon')
+where name not like '%mon'
+
+-- Modify your inserted animals to include owner information (owner_id):
+-- Sam Smith owns Agumon.
+
+update animals
+set owner_id=(select id from owners where full_name='Sam Smith ')
+where name='Agumon'
+
+-- Jennifer Orwell owns Gabumon and Pikachu.
+update animals
+set owner_id=(select id from owners where full_name='Jennifer Orwell')
+where name IN ('Gabumon', 'Pikachu')
+
+-- Bob owns Devimon and Plantmon.
+update animals
+set owner_id=(select id from owners where full_name='Bob')
+where name='Devimon' or name='Plantmon'
+
+-- Melody Pond owns Charmander, Squirtle, and Blossom.
+update animals
+set owner_id=(select id from owners where full_name='Melody Pond')
+where name IN ('Charmander', 'Squirtle', 'Blossom')
+
+
+-- Dean Winchester owns Angemon and Boarmon.
+update animals
+set owner_id=(select id from owners where full_name='Dean Winchester')
+where name IN ('Angemon', 'Boarmon')
+
+
 
--- schema.sql
+++ schema.sql
@@ -10,3 +10,37 @@ CREATE TABLE animals
 
 ALTER TABLE animals
     ADD COLUMN species text;
+    
+    -- primary key to animals table
+
+    ALTER TABLE animals ADD primary key (id);	
+
+    -- altering id datatype
+    ALTER TABLE animals
+ALTER COLUMN id TYPE integer GENERATED ALWAYS AS IDENTITY;
+
+-- remove species colum
+ALTER TABLE animals DROP COLUMN species;
+
+-- Add column species_id which is a foreign key referencing species table
+ALTER TABLE animals
+ADD COLUMN species_id INT REFERENCES species(id);
+
+-- Add column owner_id which is a foreign key referencing the owners table
+ALTER TABLE animals
+ADD COLUMN owner_id  INT REFERENCES owners(id);
+
+
+
+--  owners table
+Create TABLE owners(
+id SERIAL primary key,
+full_name text,
+age integer
+)
+
+--   species table
+CREATE TABLE species(
+    id SERIAL PRIMARY KEY,
+    name string
+)
